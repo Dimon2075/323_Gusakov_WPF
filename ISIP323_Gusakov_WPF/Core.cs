17735ab48e8429517bceb6a6be5e9923ec8ec85e
Dimon2075 <dmitriy.gusakov.2006@mail.ru>
2026-03-12 11:18:17 +0300

    бд

---
+++ ISIP323_Gusakov_WPF/Core.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP323_Gusakov_WPF
+{
+    public class Core
+    {
+        private static Entities4 _db;
+
+        public static Entities4 DB
+        {
+            get
+            {
+                if (_db == null)
+                    _db = new Entities4();
+                return _db;
+            }
+        }
+
+    }
+}
