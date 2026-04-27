6144a60b9ad0031a83a71c3cff20db21dc928862
Dimon2075 <dmitriy.gusakov.2006@mail.ru>
2026-04-27 12:07:52 +0300

    Создание ветви. Подключение к бд.

---
+++ ISIP323_Gusakov_WPF/Core.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP323_Gusakov_WPF
+{
+    public class Core
+    {
+        private static MAGAZ_CosmosEntities _db;
+        public static MAGAZ_CosmosEntities DB => GetContext();
+        public static MAGAZ_CosmosEntities GetContext()
+        {
+            if( _db == null )
+            {
+                _db = new MAGAZ_CosmosEntities();
+            }
+            return _db;
+        }
+        public static Users AuthUser { get; set; }
+        public static List<Products> SelectedProducts = new List<Products>();
+    }
+}
